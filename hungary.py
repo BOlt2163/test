db17adf31ad83db54d35b3f093acda94cb283ff2
himanshu <appchhavilap@appChhavilaps-MacBook-Air.local>
2022-03-13 18:20:46 +0530

    chenges in a code

--- hungary.py
+++ hungary.py
@@ -1,3 +1,14 @@
-print("Hello world")
-print("this is my code for git hub")
-#### hi everyone
+import turtle
+wn = turtle.Screen()        # creates a graphics window
+wn.setup(540,508)           # set window dimension
+
+alex = turtle.Turtle()      # create a turtle named alex
+alex.shape("turtle")        # alex looks like a turtle
+alex.color('red')           # alex has a color
+
+
+alex.circle(50)              # draws a circle of radius 50
+alex.backward(50)            # alex moves 50 positions backward
+alex.forward(100)             # alex moves 50 positions forward
+alex.right(60)               # alex turns 60 degrees right
+alex.left(60)
