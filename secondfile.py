0c91a173f6f00b6d2f8fa5ea16b14f9884440213
himanshu <appchhavilap@appChhavilaps-MacBook-Air.local>
2022-04-01 12:00:09 +0530

    Break and continue

--- secondfile.py
+++ secondfile.py
@@ -1,6 +1,14 @@
-start=1
-end=10
-step=2
-for number in range (start, end, step):
-    print("The current number is ", number)
+for passenger in "A", "A", "FC", "C", "FA", "SP", "A", "A":
+    if (passenger == "FC" or passenger == "FA"):
+        print("No check required")
+        continue
 
+    if (passenger == "SP"):
+        print("Declare emergency in the airport")
+        break
+
+    if (passenger == "A" or passenger == "C"):
+        print("Proceed with normal security check")
+
+    print("Check the person")
+    print("Check for cabin baggage")
